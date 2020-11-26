d03b2ee55b66fd3e1ac82c3e07acbb6a24dfc006
“brunobatista” <brunobatista101@gmail.com>
2020-11-26 18:38:50 -0300

    teste

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -6,10 +6,12 @@ from tech_news.collector.scrapper import fetch_content
 from pymongo import MongoClient
 from decouple import config
 
-DB_HOST = config("DB_HOST", default="mongodb")
+
+DB_HOST = config("DB_HOST", default="localhost")
 DB_PORT = config("DB_PORT", default=27017)
 
 client = MongoClient(host=DB_HOST, port=int(DB_PORT))
+
 db = client.tech_news
 
 NEW_NOTICE = {'url': 'https://www.tecmundo.com.br/brincadeira-levadaserio.htm',

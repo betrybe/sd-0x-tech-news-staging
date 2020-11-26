51cd13cc847c351ffa9469a5d23430c03d1cdc53
“brunobatista” <brunobatista101@gmail.com>
2020-11-26 16:41:15 -0300

    tudo passando

---
+++ tech_news/analyzer/search_service.py
@@ -0,0 +1,10 @@
+from tech_news.database import search_news
+
+
+def get_news_list(query):
+    news_list = []
+    news = search_news(query)
+    for each in news:
+        new =  (each["title"], each["url"])
+        news_list.append(new)
+    return news_list
